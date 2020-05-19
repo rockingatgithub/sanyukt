1c8b9c6c3a56d6e1218c376d0fa4e0496c8c1862
rockingatgithub <contactwithsid@gmail.com>
2020-05-20 01:55:47 +0530

    static files added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
 
 //use express-ejs-layouts..
 app.use(expressLayouts);
