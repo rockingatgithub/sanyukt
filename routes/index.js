bfe2644c3406a0356c41ede48d27f5eaf3ccd322
rockingatgithub <contactwithsid@gmail.com>
2020-05-20 00:09:21 +0530

    user and profile route added

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
 router.get('/', homeController.home);
+router.use('/users', require('./users'));  // as this file is used as main file 
+
+
+
 
 console.log('route is working');
 
