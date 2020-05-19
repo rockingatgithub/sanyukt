e2ab9e5bf2167d064115361dae731dd5d724a031
rockingatgithub <contactwithsid@gmail.com>
2020-05-19 23:15:23 +0530

    setting directory structures

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err)
+    {
+        console.log(`error running server on port: ${port}`);
+    }
+    console.log(`Server is running on port: ${port}`);
+})
