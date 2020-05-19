12762da57491c4dd9c87718958378f178bde20e9
rockingatgithub <contactwithsid@gmail.com>
2020-05-20 00:17:16 +0530

    view engine added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const port = 8000;
 //use the router..
 app.use('/', require('./routes'));
 
-
+//setup the views engines..
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.listen(port, function(err){
     if(err)
     {
