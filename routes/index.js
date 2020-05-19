71dd47fdd25c35d1e98ef12b465cac1a5c1cc984
rockingatgithub <contactwithsid@gmail.com>
2020-05-19 23:50:12 +0530

    home controller added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const homeController = require('../controllers/home_controller');
+router.get('/', homeController.home);
+
 console.log('route is working');
 
 module.exports = router ;
