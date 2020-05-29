083169bded60199b893c4629e20eae50f3a91b4d
rockingatgithub <contactwithsid@gmail.com>
2020-05-30 00:54:08 +0530

    added cookie parser

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db=require('./config/mongoose');
+
+
+//middlewares below
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+
 app.use(express.static('./assets'));
 //extract syles and scripts..
 app.set('layout extractStyles', true);
