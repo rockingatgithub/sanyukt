bd9be5e37eef97b44cec7c912c32f13be031ee39
rockingatgithub <contactwithsid@gmail.com>
2020-06-04 01:34:21 +0530

    delete comments feature added

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,22 @@ module.exports.create = function(req, res){
         }
     })
 }
+
+//deleting a comment....
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id, function(err, comment){
+        if(comment.user == req.user.id){
+            let postId = comment.post;  //saving the post id in order to remove from array stored in post...
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err, post){
+                return res.redirect('back');
+            })
+
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
+}
