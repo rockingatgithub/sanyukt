30f92511cc79295c54a04181958f36364440fb04
rockingatgithub <contactwithsid@gmail.com>
2020-06-16 23:49:56 +0530

    sockets configured

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://54.90.38.141:5000');
+        this.socket = io.connect('http://localhost:5000');
 
         if(this.userEmail){
             this.connectionHandler();
